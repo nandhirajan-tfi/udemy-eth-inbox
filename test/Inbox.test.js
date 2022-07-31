113b04380ebb6f40dbf212658170a78716f6cfc4
Nandhirajan Pandiaraj <nandy@trans-fi.xyz>
2022-07-31 16:52:26 +0530

    Deployment with Web3

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,18 +3,25 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
 
 let accounts;
+let inbox;
+
 /**
  * Get a list of all accounts
  * Use one of the accounts to deploy the contract
  */
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ["Hi"] })
+        .send({ from: accounts[0], gas: "1000000" })
 })
 
 describe("Inbox", () => {
     it("Deploys Contract", () => {
-        console.log(accounts);
+        console.log(inbox);
     });
 })

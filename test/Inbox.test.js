e3643e5798dfceeedb7f80d768d7b315ede00fa2
Nandhirajan Pandiaraj <nandy@trans-fi.xyz>
2022-07-31 16:34:30 +0530

    Refactor to async/await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,19 +4,17 @@ const Web3 = require("web3");
 
 const web3 = new Web3(ganache.provider());
 
+let accounts;
 /**
  * Get a list of all accounts
  * Use one of the accounts to deploy the contract
  */
-beforeEach(() => {
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        })
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
 })
 
 describe("Inbox", () => {
     it("Deploys Contract", () => {
-
+        console.log(accounts);
     });
 })

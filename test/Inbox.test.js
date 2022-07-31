8969d72033b37da0af33b84ef5ba3e3e4d183db8
Nandhirajan Pandiaraj <nandy@trans-fi.xyz>
2022-07-31 17:20:45 +0530

    Assert - Verifying the initial message to the contract constructor

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,6 +7,7 @@ const { interface, bytecode } = require("../compile");
 
 let accounts;
 let inbox;
+const INITIAL_STRING = "Hi";
 
 /**
  * Get a list of all accounts
@@ -16,7 +17,7 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
     inbox = await new web3.eth.Contract(JSON.parse(interface)) // Teaches Web3 about what methods an Inbox contract has
-        .deploy({ data: bytecode, arguments: ["Hi"] }) // Tells Web3 that we want to deploy a new copy of this contract
+        .deploy({ data: bytecode, arguments: [INITIAL_STRING] }) // Tells Web3 that we want to deploy a new copy of this contract
         .send({ from: accounts[0], gas: "1000000" }) // Instructs Web3 to send out a transaction that creates this contract
 })
 
@@ -24,4 +25,9 @@ describe("Inbox", () => {
     it("Deploys Contract", () => {
         assert.ok(inbox.options.address);
     });
+
+    it("Contract has the default message", async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING);
+    })
 })
